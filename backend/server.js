560dbaa26c87724d724d1bbebc441273cf3481bb
khushigiri <02khushi.giri@gmail.com>
2026-02-20 21:12:21 +0530

    Started

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/tasks", require("./routes/taskRoutes"));
+
+app.listen(5000, () => console.log("Server running"));
